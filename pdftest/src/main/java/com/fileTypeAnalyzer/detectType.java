004f9e2b44cfd27211a146152501b5fc657a48d3
Nikita Zakharov <Nikita.Zaharov@leroymerlin.ru>
2022-05-17 17:53:36 +0300

    add file type analyzer

---
+++ pdftest/src/main/java/com/fileTypeAnalyzer/detectType.java
@@ -0,0 +1,72 @@
+package pdftest.src.main.java.com.fileTypeAnalyzer;
+import java.io.*;
+import java.util.Arrays;
+
+public class detectType {
+
+    public static int[] computePrefix(byte[] input) {
+        int[] pi = new int[input.length];
+
+        int k = 0;
+        for(int q = 2; q < input.length; q++) {
+            while (k > 0 && input[k] != input[q]) {
+                k = pi[k];
+            }
+            if (input[k] == input[q]) {
+                k = k + 1;
+            }
+            pi[q] = k;
+        }
+        return pi;
+    }
+
+    public static boolean findKMP(byte [] buffer, byte [] key) {
+        int[] prefix = computePrefix(key);
+        int range = 0;
+        int len = key.length;
+        boolean result = false;
+        int start = 0;
+        int end = key.length;
+        while (end <= buffer.length) {
+            byte[] subarray =  Arrays.copyOfRange(buffer, start, end);
+            if (Arrays.equals(subarray, key)) {
+                result = true;
+                break;
+            } else {
+                for (int k = len-1; k >= 0; k--) {
+                    if (Arrays.equals(Arrays.copyOfRange(subarray, 0, k), Arrays.copyOfRange(key, 0, k))) {
+                        range = k;
+                        break;
+                    }
+                    start = start + range - prefix[k];
+                    end = end + range - prefix[k];
+                    range = 0;
+                }
+                return result;
+            }
+
+
+
+
+        }
+        return result;
+    }
+
+
+    public static boolean detector(String stringName, String fileTypePattern) throws IOException {
+
+        File file = new File(stringName);
+        byte[] inputArg = fileTypePattern.getBytes();
+        byte[] bytes = new byte[(int) file.length()];
+
+        try(FileInputStream fis = new FileInputStream(file)){
+            fis.read(bytes);
+            if (findKMP(bytes, inputArg)) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+    }
+}
--- pdftest/src/main/java/com/start/Main.java
+++ pdftest/src/main/java/com/start/Main.java
@@ -2,28 +2,37 @@ package com.start;
 import com.Parsers.RtfParser;
 import com.Parsers.HtmlParser;
 import com.Parsers.Parser;
+import pdftest.src.main.java.com.fileTypeAnalyzer.detectType;
 import javax.swing.text.BadLocationException;
 import java.io.IOException;
 
 
 
 public class Main {
-    public static String Detect_type(String file_name) {
+    public static String detectType(String file_name) throws IOException {
 //        TODO need to write type detector
-        return "None";
+        String fileType = null;
+        detectType detect = new detectType();
+        if (detect.detector(file_name, "{\\rtf")) fileType = "rtf";
+        else if (detect.detector(file_name, "<!doctype>")) fileType = "html";
+
+        return fileType;
     }
 
 
     public static void main(String[] args) throws BadLocationException, IOException {
         String type_of_file = "";
         for (String file_name : args) {
-            type_of_file = Detect_type(file_name);
+            type_of_file = detectType(file_name);
+            System.out.println(type_of_file);
             if (type_of_file.equals("html")) {
                 Parser technology = new HtmlParser();
                 technology.Parse(file_name);
             } else if (type_of_file.equals("rtf")) {
                 Parser technology = new RtfParser();
                 technology.Parse(file_name);
+            } else if (type_of_file.equals(null)) {
+                System.out.println("input is not html or rtf file");
             }
         }
     }
