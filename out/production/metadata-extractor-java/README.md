d544048fb4d035a6399c86ea1834c6863b3b4f7c
Fancy2000 <vip_dzhikaevi@mail.ru>
2022-05-17 18:28:46 +0300

    add traversal within a folder

---
+++ out/production/metadata-extractor-java/README.md
@@ -0,0 +1,10 @@
+# metadata-extractor-java
+## Инструкция по запуску
+```bash
+cd pdftest
+mvn install
+mvn compile
+java -jar target/jumpstart-1.0-SNAPSHOT.jar ../test.rtf
+```
+
+[CRC card](CRC-cards.md)
--- pdftest/src/main/java/com/start/Main.java
+++ pdftest/src/main/java/com/start/Main.java
@@ -4,26 +4,81 @@ import com.Parsers.HtmlParser;
 import com.Parsers.Parser;
 import javax.swing.text.BadLocationException;
 import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import com.fileTypeAnalyzer.detectType;
 
 
 
 public class Main {
     public static String Detect_type(String file_name) {
 //        TODO need to write type detector
-        return "None";
+//        String fileType = null;
+//        detectType detect = new detectType();
+//        try {
+//            if (detect.detector(file_name, "{\\rtf"))
+//                fileType = "rtf";
+//            else if (detect.detector(file_name, "<!doctype>"))
+//                fileType = "html";
+//        } catch (IOException e) {
+//            System.out.println("IOException occurred when extracting text from HTML file: " + file_name);
+//        }
+//        return fileType;
+        if (file_name.substring(file_name.indexOf("."), file_name.length()).equals(".html")) {
+            return "html";
+        } else if  (file_name.substring(file_name.indexOf("."), file_name.length()).equals(".rtf")) {
+            return "rtf";
+        }
+        return "";
+    }
+
+
+    public static void searchFiles(File rootFile, List<File> fileList) {
+        if (rootFile.isDirectory()) {
+            File[] directoryFiles = rootFile.listFiles();
+            if (directoryFiles != null) {
+                for (File file: directoryFiles) {
+                    if (file.isFile()) {
+                        fileList.add(file);
+                    }
+                }
+            }
+        }
+
     }
 
 
     public static void main(String[] args) throws BadLocationException, IOException {
+        ArrayList<File> fileList = new ArrayList<>();
+        File pared_dir = new File(args[0]+"/Parsed");
+        File root = new File(args[0]);
+        if (!pared_dir.exists()) {
+            pared_dir.mkdir();
+        }
+        searchFiles(root, fileList);
+        if (fileList.size() == 0) {
+            return;
+        }
         String type_of_file = "";
-        for (String file_name : args) {
-            type_of_file = Detect_type(file_name);
+        boolean parsed = false;
+        for (File file_name : fileList) {
+            System.out.println(file_name.getName());
+            type_of_file = Detect_type(file_name.getName());//should be file_name.getAbsolutePath()!!!!!!!
+            System.out.println(type_of_file);
             if (type_of_file.equals("html")) {
                 Parser technology = new HtmlParser();
-                technology.Parse(file_name);
+                technology.Parse(file_name.getAbsolutePath());
+                parsed = true;
             } else if (type_of_file.equals("rtf")) {
                 Parser technology = new RtfParser();
-                technology.Parse(file_name);
+                technology.Parse(file_name.getAbsolutePath());
+                parsed = true;
+            }
+            if (parsed) {
+                System.out.println("in cond to moove file in parsed dir");
+                parsed = false;
+                file_name.renameTo(new File(pared_dir, file_name.getName()));
             }
         }
     }
