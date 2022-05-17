0eb75210349427133c77e70b9119bd3f7a4ec660
Fancy2000 <vip_dzhikaevi@mail.ru>
2022-05-17 21:39:29 +0300

    add result file

--- pdftest/src/main/java/com/Parsers/RtfParser.java
+++ pdftest/src/main/java/com/Parsers/RtfParser.java
@@ -3,6 +3,8 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultStyledDocument;
 import javax.swing.text.rtf.RTFEditorKit;
 import com.Parsers.Parser;
+import org.jsoup.nodes.Element;
+
 import java.io.*;
 
 
@@ -14,6 +16,11 @@ public class RtfParser implements Parser {
             styledDoc = new DefaultStyledDocument();
             InputStream is = new FileInputStream(file_name);
             new RTFEditorKit().read(is, styledDoc, 0);
+            FileWriter writer = new FileWriter("result.txt", true);
+            writer.write("\n------------------------------- " + file_name + " -------------------------------\n");
+            writer.write(styledDoc.getText(0, styledDoc.getLength()));
+            writer.write("\n--------------------------------------------------------------\n");
+            writer.close();
 
         } catch (BadLocationException e) {
             System.out.println("BadLocationException occurred when extracting text from RTF file:" + file_name);
